045f2a7ef3dcbf4e1ca75998c409138d460f4676
CarlosGonzález20 <ll3224645@gmail.com>
2024-10-13 13:27:40 -0600

    cambiando botnes y referencias para utilizar la appi del servidor con express

--- FrontEnd/prueba-api/src/App.js
+++ FrontEnd/prueba-api/src/App.js
@@ -4,20 +4,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <head>
+            <title>Bienvenido a la API</title>
+        </head>
+        <body>
+            <header>
+                <h1>Bienvenido a nuestra API</h1>
+            </header>
+            <p>Explora las funcionalidades de nuestra API. Aquí puedes gestionar productos, obtener información detallada y mucho más.</p>
+            <p>Visita nuestras rutas disponibles:</p>
+            <div className="buttons">
+                <button><a href="http://localhost:3001/api/usuarios">Ver todos los usuarios</a></button>
+                <button><a href="http://localhost:3001/api/autenticar">Login</a></button>
+                <button><a href="http://localhost:3001/api/nuevoUsuario">Insetar nuevo usuario</a></button><br></br>
+                <button><a href="http://localhost:3001/api/actualizarUsuario">Actualizar usuario</a></button>
+                <button><a href="http://localhost:3001/api/borrarUsuario">Borrar usuario</a></button>
+            </div>
+            <footer>
+                <p>&copy; 2024 Nuestra API de Productos</p>
+            </footer>
+        </body>
     </div>
   );
 }
